92fdf51f702369347471a2099c53e2570dc8cc0a
Miguel <mikeluxex@gmail.com>
2024-02-04 00:13:34 -0500

    Mi primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'sanFrancisco': "url('../img/sanFrancisco.jpg')",
+        'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
+        'bali': "url('../img/bali.jpg')",
+        'chicago': "url('../img/chicagp.jpg')",
+        'iceland': "url('../img/iceland.jpg')",
+        'LA': "url('../img/LA.jpg')",
+        'miami': "url('../img/miami.jpg')",
+        'newYork': "url('../img/new_york.jpg')",
+        'norway': "url('../img/norway.jpg')",
+        'seattle': "url('../img/seattle.jpg')",
+        'switzerland': "url('../img/switzerland.jpg')",
+        'sydney': "url('../img/sydney.jpg')",
+        'yosemite': "url('../img/yosemite.jpg')"
+      },
+      backgroundColor : theme => ({
+        ...theme('colors'),
+        'primary' : '#CC2D4A',
+        'secondary' : '#8FA206',
+        'terciary' : '#61AEC'
+      }),
+      textColor : {
+        'primary' : '#CC2D4A',
+        'secondary' : '#8FA206',
+        'terciary' : '#61AEC'
+      }
+    },
+  },
+  plugins: [],
+}
+
